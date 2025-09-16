4a9c3eacd6ee3c1d4170e2506276d257f94cef10
ayalaI <ayala6763638@gmail.com>
2025-09-16 15:06:54 +0300

    Your commit message

---
+++ eventserveraylal/Controllers/EventController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eventserveraylal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public static List<Event> Events = new List<Event> { new Event { id = 1, description = "Wedding", end = new DateTime(), start = new DateTime() },
+                 new Event{ id = 2, description = "Bar Miztva", start = new DateTime(), end = new DateTime()} };        // GET: api/<EventController>
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return Events;
+        }
+
+        // GET api/<EventController>/5
+        [HttpGet("{id}")]
+        public Event Get(int id)
+        {
+            return Events.Find(e => e.id == id);
+        }
+
+        // POST api/<EventController>
+        [HttpPost]
+        public void Post([FromBody] Event value)
+        {
+            Events.Add(value);
+        }
+
+        // PUT api/<EventController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Event value)
+        {
+            var index = Events.FindIndex(e=> e.id==id);
+            Events[index].description = value.description;
+            Events[index].id = value.id;
+            Events[index].end = value.end;
+            Events[index].start = value.start;
+
+        }
+
+        // DELETE api/<EventController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var element1 = Events.Find(e => e.id == id);
+            Events.Remove(element1);
+        }
+    }
+}
---
+++ eventserveraylal/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace eventserveraylal
+{
+    public class Event
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end  { get; set; }
+
+    }
+}
